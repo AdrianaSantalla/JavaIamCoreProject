ff895ccede77b1624264e15a0689561c4cedad1f
AdrianaSantalla <adriana.santalla@gmail.com>
2016-01-29 16:19:38 +0100

    adding main class

---
+++ IamCoreProject/src/fr/epita/iamcoreproject/launcher/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fr.epita.iamcoreproject.launcher;
+
+/**
+ * @author Usuario
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
