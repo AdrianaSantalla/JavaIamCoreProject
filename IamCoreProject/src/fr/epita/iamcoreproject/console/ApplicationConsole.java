0f4877102928d6e603f96e1ef0166d49cf9af596
AdrianaSantalla <adriana.santalla@gmail.com>
2016-02-06 13:50:58 +0100

    javadoc

--- IamCoreProject/src/fr/epita/iamcoreproject/console/ApplicationConsole.java
+++ IamCoreProject/src/fr/epita/iamcoreproject/console/ApplicationConsole.java
@@ -6,67 +6,70 @@ package fr.epita.iamcoreproject.console;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import fr.epita.iamcoreproject.dao.IdentityDAOInterface;
-import fr.epita.iamcoreproject.dao.IdentityXmlDAO;
-import fr.epita.iamcoreproject.datamodel.Identity;
-
 /**
- * This is the main Console from the Application.
- * <p>Here we first call the <code>AuthenticateActionConsole</code> method <code>execute()</code> 
+ * This is the main Console from the Application to manage Identities.
+ * <p>First the <code>AuthenticateActionConsole</code> method <code>execute()</code> is called 
  * because the first action to perform is the authentication for the user.
- * If the authentication is successful, the Menu is shown with the function <code>showMenu()</code>.
+ * If the authentication is <b>successful</b>, the Menu is shown with the function <code>showMenu()</code>.
  * 
- * <p>We ask the user to select the action that he wants to execute. According to the selected option,
- * we call the corresponding type of <code>ConsoleAction</code>. For example, if the user selected the 
- * create option, we call the corresponding Action, creating an instance of this class:
+ * <p>The user is asked to select an option from the Menu. According to the selected option,
+ * the corresponding type of <code>ConsoleAction</code> is called. For example, if the user selected the 
+ * <b>create option</b>, the CreateActionConsole is called, creating an instance of this class:
  * <blockquote><pre>{@code
  *     new CreateActionConsole().execute();
  * }</pre></blockquote>
- *  
- * <p>This  
  * 
+ * <p>In the same way, the other ActionConsoles related to the selected option are called. 
+ *   
  * @author Adriana Santalla and David Cechak
- *
+ * @version 1;
  */
 public class ApplicationConsole {
-	
+	//scanner to read the option selected by the user
 	Scanner scanner = new Scanner(System.in);
-	
+	/**
+	 * Method to execute the <code>ApplicationConsole</code>
+	 */
 	public void run() {
 		System.out.println("Welcome to the Identities Application Management");
+		//call to the Authentication process
 		new AuthenticateActionConsole().execute();
 		int selectedOption=-1;
+		//do while loop to keep asking the user the option until he chooses the option 0
 		do{
 			showMenu();
+			//try catch to verify if the inserted value is an integer value
 			try {
 	    		selectedOption = scanner.nextInt();
 	    	} catch (InputMismatchException e) {
+	    		//if not we print this error message and let the user to insert a valid option
 	    	    System.err.println("Please input a number: ");
 	    	    scanner.nextLine();
 	    	    continue;
 	    	}
 			scanner.nextLine();
+			//Switch case to find the option selected by the user
 			switch(selectedOption){
-			case 1: 
+			case 1://Show all option 
 				new ReadAllActionConsole().execute();
 				break;
-			case 2: 
+			case 2: //Create option
 				new CreateActionConsole().execute();
 				break;
-			case 3: 
+			case 3: //Update option
 				new UpdateActionConsole().execute();
 				break;
-			case 4: 
+			case 4: //Delete option
 				new DeleteActionConsole().execute();
 				break;
-			case 5: 
+			case 5: //Search option
 				new SearchActionConsole().execute();
 				break;
-			case 0: 
+			case 0: //Exit option
 				scanner.close();
 				System.out.println("Goodbye!");
 				break;
-			default:
+			default: //When the inserted value not corresponds to any option although it is numeric
 				System.out.println("Invalid option");
 				break;
 			}
@@ -74,8 +77,10 @@ public class ApplicationConsole {
 			}
 			while(selectedOption!=0);					
 	}
-	
-	private static void showMenu(){
+	/**
+	 * Internal method to print the menu.
+	 */
+	private void showMenu(){
 		// Display menu
 	    System.out.println("____________________________");
 	    System.out.println("|       MAIN MENU          |");
--- IamCoreProject/src/fr/epita/iamcoreproject/console/AuthenticateActionConsole.java
+++ IamCoreProject/src/fr/epita/iamcoreproject/console/AuthenticateActionConsole.java
@@ -10,30 +10,60 @@ import fr.epita.iamcoreproject.dao.IdentityXmlDAO;
 import fr.epita.iamcoreproject.datamodel.Identity;
 
 /**
- * @author Usuario
- *
+ * This class <code>AuthenticateActionConsole</code> implements the interface <code>ActionConsole</code> and
+ * thus it has the <code>execute()</code> method.
+ * 
+ * <p>In this class, first the username and password of the user trying to execute the application, are asked.
+ *  
+ * <p>In this project data is persisted in an XML file.This is possible using the <code>IdentityXmlDAO</code>
+ * class.
+ *  
+ * <blockquote><pre>{@code
+ *     IdentityDAOInterface dao = new IdentityXmlDAO();
+ * }</pre></blockquote>
+ * 
+ * <p>This instance allows the access to the data. Here the function <code>dao.findIdentity(user)</code> 
+ * is used to compare username password matching. 
+ *  
+ * @see fr.epita.iamcoreproject.dao.IdentityXmlDAO 
+ * @author Adriana Santalla and David Cechak
+ * @version 1;
  */
 public class AuthenticateActionConsole implements ActionConsole{
-	
+	//scanner to read the username and password from the user
 	Scanner scanner = new Scanner(System.in);
-	Identity currentUser;
+	
+	/**
+	 * Method to execute the <code>AuthenticateActionConsole</code>
+	 * <p>An important assumption here, is that the <b>displayName</b> property from <code>Identity</code> is
+	 * considered as the <b>username</b> and that we just let Identities with the property type="admin" to
+	 * access the application.
+	 */
 	@Override
 	public void execute() {
+		//Creating an instance of the DAO that will provide access to the data storage
 		IdentityDAOInterface dao = new IdentityXmlDAO();
 		boolean state = false;
+		//while loop to keep asking the user an username and password when the authentication fails
 		while(!state){
+			//Asking for user input
 			System.out.println("\nAuthentication");
 			System.out.println("Please enter your username: ");
 			String username = scanner.nextLine();
 			System.out.println("Please enter your password: ");
 			String password = scanner.nextLine();
+			//creating a new identity to find it
 			Identity user = new Identity();
+			//here we use the property displayName for the username
 			user.setDisplayName(username);
+			//checking if the given username exits in the XML file
 			if(!dao.findIdentity(user).isEmpty())
 			{
-				currentUser = dao.findIdentity(user).get(0);
-				if(currentUser.getType().equals("admin")) {
-					if(currentUser.getDisplayName().equals(username) && currentUser.getPassword().equals(password)) {
+				//getting the first user in the resulting list 
+				user = dao.findIdentity(user).get(0);
+				//verifying if it is an admin user
+				if(user.getType().equals("admin")) {
+					if(user.getDisplayName().equals(username) && user.getPassword().equals(password)) {
 						System.out.println("\nWelcome "+username);
 						state = true;
 					}
@@ -51,8 +81,6 @@ public class AuthenticateActionConsole implements ActionConsole{
 				System.out.println("\nUnexisting user");
 				state = false;
 			}
-		}
-		
+		}		
 	}
-
 }
